ecabc321e83bce00d16c38f66921491225e56bb2
Lane Olson <laneolson@gmail.com>
2020-10-28 09:41:58 -0700

    add useWindowSize to exports

--- packages/hooks/src/index.js
+++ packages/hooks/src/index.js
@@ -1,7 +1,6 @@
-export { default as useDebounce } from './useDebounce'
-export {
-  default as useDidUpdateEffect
-} from './useDidUpdateEffect'
-export { default as useInterval } from './useInterval'
-export { default as useKeyPress } from './useKeyPress'
-export { default as usePrevious } from './usePrevious'
+export { default as useDebounce } from "./useDebounce";
+export { default as useDidUpdateEffect } from "./useDidUpdateEffect";
+export { default as useInterval } from "./useInterval";
+export { default as useKeyPress } from "./useKeyPress";
+export { default as usePrevious } from "./usePrevious";
+export { default as useWindowSize } from "./useWindowSize";
